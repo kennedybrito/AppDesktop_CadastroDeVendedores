5998d284c032d86b7cbc1be2437e091ee8d951f9
Kennedy Brito <brito.kennedy10@gmail.com>
2022-06-30 20:27:03 -0300

    acesso ao banco de dados

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,17 @@
 module crudvendedores {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
+	
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui;
 	opens gui.util;
 	opens model.entities;
 	opens model.services;
+	opens db;
+	opens model.dao.impl;
+	opens model.dao;
+	
 	
 }
